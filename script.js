aa12f8089026956c3e7ea917efbf4e9bd4e30202
Dhruv <96535287+Lucifer7990@users.noreply.github.com>
2022-01-25 20:39:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+var imgs = document.getElementById("imgs").children;
+var searchElm = document.getElementById("search-elm");
+
+
+function tap() {
+    var block = document.getElementById("imgs");
+    block.style.display = 'none'
+    searchElm.style.display = 'flex';
+    var search = document.getElementById("search").value;
+    searchElm.innerHTML = '';
+    if(search == ''){
+        block.style.display = 'flex';
+        searchElm.style.display = 'none';
+    }
+    var j=0;
+    for(let i=0;i<imgs.length;i++){
+        if(imgs[i].name.indexOf(`${search}`) != -1){
+            var sElm = document.createElement('img');
+            sElm.setAttribute("src",imgs[i].src);
+            searchElm.appendChild(sElm);
+            j++;
+        }
+    }
+    if(j==0){
+        alert("no match found");
+        block.style.display = 'flex';
+        searchElm.style.display = 'none';
+    }
+}
+
+
+
+
+
